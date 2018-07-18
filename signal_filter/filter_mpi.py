6794e8c9c4d087e8349c763ce129dd21bb18b616
ssomnath <somnaths@ornl.gov>
2018-07-18 12:10:39 -0400

    Changed the import statements. and simplified last portion of script

--- signal_filter/filter_mpi.py
+++ signal_filter/filter_mpi.py
@@ -1,10 +1,12 @@
 import h5py
 from mpi4py import MPI
-from signal_filter.fft import LowPassFilter
-from signal_filter.mpi_signal_filter import SignalFilter
+from fft import LowPassFilter
+from mpi_signal_filter import SignalFilter
 
 h5_path = 'giv_raw.h5'
 h5_f = h5py.File(h5_path, mode='r+', driver='mpio', comm=MPI.COMM_WORLD)
+# Not necessary I think but Chris used it
+h5_f.atomic = True # This doesn't seem to make any difference
 
 h5_grp = h5_f['Measurement_000/Channel_000']
 h5_main = h5_grp['Raw_Data']
@@ -20,13 +22,4 @@ sig_filt = SignalFilter(h5_main, frequency_filters=frequency_filters,
                            write_condensed=False, num_pix=1, verbose=False)
 h5_filt_grp = sig_filt.compute()
 
-# RuntimeError: Can't decrement id ref count (MPI_ERR_ARG: invalid argument of some other kind)
-MPI.COMM_WORLD.barrier()
 h5_f.close()
-"""
-try:
-    h5_f.close()
-    print('Rank {} managed to close'.format(MPI.COMM_WORLD.Get_rank()))
-except RuntimeError:
-    print('Rank {} experienced a run-time error when closing the file'.format(MPI.COMM_WORLD.Get_rank()))
-"""
