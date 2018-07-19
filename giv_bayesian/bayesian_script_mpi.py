7667da04f1f7128839bb940a85ae8b87b08d4f88
ssomnath <somnaths@ornl.gov>
2018-07-19 09:10:52 -0400

    Configured the mpi script to match the serial script

--- giv_bayesian/bayesian_script_mpi.py
+++ giv_bayesian/bayesian_script_mpi.py
@@ -1,28 +1,36 @@
-import h5py
-from mpi4py import MPI
-from giv_bayesian_mpi import GIVBayesian
+import multiprocessing
 
-h5_path = 'giv_raw.h5'
-h5_f = h5py.File(h5_path, mode='r+', driver='mpio', comm=MPI.COMM_WORLD)
-
-h5_grp = h5_f['Measurement_000/Channel_000']
-
-ex_freq = h5_grp.attrs['excitation_frequency_[Hz]']
-
-h5_resh = h5_f['Measurement_000/Channel_000/Raw_Data-FFT_Filtering_000/Filtered_Data-Reshape_000/Reshaped_Data']
-
-i_cleaner = GIVBayesian(h5_resh, ex_freq, 9, r_extra=110, num_x_steps=250, cores=3,
-                           max_mem_mb=1024*10, verbose=True)
-h5_bayes_grp = i_cleaner.compute()
-
-# RuntimeError: Can't decrement id ref count (MPI_ERR_ARG: invalid argument of some other kind)
-MPI.COMM_WORLD.barrier()
-print('Rank {} passed the barrier'.format(MPI.COMM_WORLD.Get_rank()))
-h5_f.close()
 """
-try:
+It is crucial that NO OTHER package be imported till the process spawning method is set.
+The next two lines must be left as is whether it is for single or multi-node computation.
+There is a bug in the way numpy is linked with accelerator libraries in Unix based systems that can sometimes
+cause multiprocessing / joblib to hang on numpy operations like dot. See these issues and links:
+https://pythonhosted.org/joblib/parallel.html#bad-interaction-of-multiprocessing-and-third-party-libraries
+https://github.com/joblib/joblib/issues/310
+https://github.com/numpy/numpy/issues/5752
+
+Solution:
+http://scikit-learn.org/stable/faq.html#why-do-i-sometime-get-a-crash-freeze-with-n-jobs-1-under-osx-or-linux
+
+So, keep everything under if __name__ == 'main'
+"""
+if __name__ == '__main__':
+    multiprocessing.set_start_method('forkserver')
+
+    import h5py
+    from mpi4py import MPI
+    from giv_bayesian_mpi import GIVBayesian
+
+    h5_path = 'giv_raw.h5'
+    h5_f = h5py.File(h5_path, mode='r+', driver='mpio', comm=MPI.COMM_WORLD)
+
+    h5_grp = h5_f['Measurement_000/Channel_000']
+
+    ex_freq = h5_grp.attrs['excitation_frequency_[Hz]']
+
+    h5_resh = h5_f['Measurement_000/Channel_000/Raw_Data-FFT_Filtering_000/Filtered_Data-Reshape_000/Reshaped_Data']
+
+    i_cleaner = GIVBayesian(h5_resh, ex_freq, 9, r_extra=110, num_x_steps=250, verbose=True)
+    h5_bayes_grp = i_cleaner.compute()
+
     h5_f.close()
-    print('Rank {} managed to close'.format(MPI.COMM_WORLD.Get_rank()))
-except RuntimeError:
-    print('Rank {} experienced a run-time error when closing the file'.format(MPI.COMM_WORLD.Get_rank()))
-"""
