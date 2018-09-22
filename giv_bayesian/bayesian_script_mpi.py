9fd22d3331f365ae7e9e9d8b9455590b024029e9
ssomnath <somnaths@ornl.gov>
2018-09-22 18:34:47 -0400

    Updated python scripts with what actually works

--- giv_bayesian/bayesian_script_mpi.py
+++ giv_bayesian/bayesian_script_mpi.py
@@ -1,28 +1,10 @@
-import multiprocessing
+import h5py
+from mpi4py import MPI
+from giv_bayesian_mpi import GIVBayesian
 
-"""
-It is crucial that NO OTHER package be imported till the process spawning method is set.
-The next two lines must be left as is whether it is for single or multi-node computation.
-There is a bug in the way numpy is linked with accelerator libraries in Unix based systems that can sometimes
-cause multiprocessing / joblib to hang on numpy operations like dot. See these issues and links:
-https://pythonhosted.org/joblib/parallel.html#bad-interaction-of-multiprocessing-and-third-party-libraries
-https://github.com/joblib/joblib/issues/310
-https://github.com/numpy/numpy/issues/5752
+h5_path = 'giv_raw.h5'
 
-Solution:
-http://scikit-learn.org/stable/faq.html#why-do-i-sometime-get-a-crash-freeze-with-n-jobs-1-under-osx-or-linux
-
-So, keep everything under if __name__ == 'main'
-"""
-if __name__ == '__main__':
-    multiprocessing.set_start_method('forkserver')
-
-    import h5py
-    from mpi4py import MPI
-    from giv_bayesian_mpi import GIVBayesian
-
-    h5_path = 'giv_raw.h5'
-    h5_f = h5py.File(h5_path, mode='r+', driver='mpio', comm=MPI.COMM_WORLD)
+with h5py.File(h5_path, mode='r+', driver='mpio', comm=MPI.COMM_WORLD) as h5_f:
 
     h5_grp = h5_f['Measurement_000/Channel_000']
 
@@ -30,7 +12,7 @@ if __name__ == '__main__':
 
     h5_resh = h5_f['Measurement_000/Channel_000/Raw_Data-FFT_Filtering_000/Filtered_Data-Reshape_000/Reshaped_Data']
 
-    i_cleaner = GIVBayesian(h5_resh, ex_freq, 9, r_extra=110, num_x_steps=250, verbose=True)
-    h5_bayes_grp = i_cleaner.compute()
+    i_cleaner = GIVBayesian(h5_resh, ex_freq, 9, r_extra=110, num_x_steps=250,
+                            max_mem_mb=None, verbose=False)
 
-    h5_f.close()
+    h5_bayes_grp = i_cleaner.compute()
